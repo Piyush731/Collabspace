d964832a7ae54ac3c3ef5ec81d78e51091bd3d10
Piyush Kashyap <PIYUSHKASHYAP3247@GMAIL.COM>
2025-06-04 17:35:54 +0530

    Update socket.js

--- backend/config/socket.js
+++ backend/config/socket.js
@@ -8,8 +8,8 @@ const typingUsers = new Map();
 module.exports = function initializeSocket(server) {
   const io = new Server(server, {
     cors: {
-      origin: ["http://localhost:3000"],
-      //origin: ["https://collabspace-one.vercel.app"],
+      //origin: ["http://localhost:3000"],
+      origin: ["https://collabspace-one.vercel.app"],
       methods: ["GET", "POST"],
       credentials: true,
     },
